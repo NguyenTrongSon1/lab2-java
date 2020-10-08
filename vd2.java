f9c278d8653e99276dc7c465d25984ee43754da8
NguyenTrongSon1 <65001546+NguyenTrongSon1@users.noreply.github.com>
2020-10-08 22:28:26 +0700

    Add files via upload

---
+++ vd2.java
@@ -0,0 +1,11 @@
+package bai2;
+
+public class vd2 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 30;
+
+        while (++num1 < -- num2);
+        System.out.println("Midpoint is : "+num1);
+    }
+}

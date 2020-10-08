a2127aabf8ddecaa337f019a4e5a4d0da68d0c27
NguyenTrongSon1 <65001546+NguyenTrongSon1@users.noreply.github.com>
2020-10-08 22:36:24 +0700

    Add files via upload

---
+++ vd3.java
@@ -0,0 +1,9 @@
+package bai2;
+
+public class vd3 {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Welcome to Loops...");
+        }
+    }
+}

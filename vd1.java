4361073a624d7faf5e84c119a1b41b1190d3b015
NguyenTrongSon1 <65001546+NguyenTrongSon1@users.noreply.github.com>
2020-10-08 22:28:05 +0700

    Add files via upload

---
+++ vd1.java
@@ -0,0 +1,15 @@
+package bai2;
+
+public class vd1 {
+    public static void main(String[] args) {
+        int num = 1;
+        int product = 0;
+
+        while(num <=5){
+            product = num * 10;
+            System.out.println("%d * 10 = %d", num, product);
+            num++;
+        }
+        System.out.println("outside the loop");
+    }
+}
